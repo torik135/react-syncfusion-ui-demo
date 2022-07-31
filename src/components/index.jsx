db4b943538ff3cc9a828144a4c556bf03da963ae
torik135 <riqibrekele@gmail.com>
2022-07-31 19:33:03 +0700

    fix components exports

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,11 @@
 export { Button } from './Button';
 export { Cart } from './Cart';
-export { Charts } from './Charts';
+
+export { LineChart } from './Charts/LineChart';
+export { Pie } from './Charts/Pie';
+export { SparkLine } from './Charts/SparkLine';
+export { Stacked } from './Charts/Stacked';
+
 export { ChartsHeader } from './ChartsHeader';
 export { Chat } from './Chat';
 export { Footer } from './Footer';
