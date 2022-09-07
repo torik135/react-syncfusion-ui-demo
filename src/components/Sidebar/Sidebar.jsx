992412849e5314437a2c392d7d0a58bd4932556a
torik135 <riqibrekele@gmail.com>
2022-09-07 18:27:40 +0700

    add: sidebar UI component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,6 +7,13 @@ import { links } from '../../data/dummyData';
 
 const Sidebar = () => {
   const activeMenu = true;
+
+  const activeLinks =
+    'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+
+  const normalLinks =
+    'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (
@@ -28,6 +35,28 @@ const Sidebar = () => {
                 <CancelIcon />
               </button>
             </TooltipComponent>
+          </div>
+          <div className='mt-10'>
+            {links.map((items) => (
+              <div key={Math.random()}>
+                <p className='text-gray-400 m-3 mt-4 uppercase'>
+                  {items.title}
+                </p>
+                {items.links.map((link) => (
+                  <NavLink
+                    className=''
+                    to={`/${link.name}`}
+                    key={`${Math.random()}_${link.name}`}
+                    onClick={({ isActive }) =>
+                      isActive ? activeLinks : normalLinks
+                    }
+                  >
+                    {link.icon}
+                    <span className='capitalize'>{link.name}</span>
+                  </NavLink>
+                ))}
+              </div>
+            ))}
           </div>
         </>
       )}
