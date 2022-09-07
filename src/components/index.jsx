66fd4564bdff4be92e768e84ff008d1090c12d42
torik135 <riqibrekele@gmail.com>
2022-09-07 17:26:07 +0700

    rename files

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,17 +1,17 @@
-export { Button } from './Button';
-export { Cart } from './Cart';
+export { Button } from './Button/Button';
+export { Cart } from './Cart/Cart';
 
-export { LineChart } from './Charts/LineChart';
-export { Pie } from './Charts/Pie';
-export { SparkLine } from './Charts/SparkLine';
-export { Stacked } from './Charts/Stacked';
+export { LineChart } from './Charts/LineChart/LineChart';
+export { Pie } from './Charts/Pie/Pie';
+export { SparkLine } from './Charts/SparkLine/SparkLine';
+export { Stacked } from './Charts/Stacked/Stacked';
 
-export { ChartsHeader } from './ChartsHeader';
-export { Chat } from './Chat';
-export { Footer } from './Footer';
-export { Header } from './Header';
-export { Navbar } from './Navbar';
-export { Notification } from './Notification';
-export { Sidebar } from './Sidebar';
-export { ThemeSetting } from './ThemeSetting';
-export { UserProfile } from './UserProfile';
+export { ChartsHeader } from './ChartsHeader/ChartsHeader';
+export { Chat } from './Chat/Chat';
+export { Footer } from './Footer/Footer';
+export { Header } from './Header/Header';
+export { Navbar } from './Navbar/Navbar';
+export { Notification } from './Notification/Notification';
+export { Sidebar } from './Sidebar/Sidebar';
+export { ThemeSetting } from './ThemeSetting/ThemeSetting';
+export { UserProfile } from './UserProfile/UserProfile';
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,18 @@
-export { Calendar } from './Calendar';
+export { Calendar } from './Calendar/Calendar';
 
-export { Area } from './Charts/Area';
-export { Bar } from './Charts/Bar';
-export { ColorMapping } from './Charts/ColorMapping';
-export { Financial } from './Charts/Financial';
-export { Line } from './Charts/Line';
-export { Pie } from './Charts/Pie';
-export { Pyramid } from './Charts/Pyramid';
-export { Stacked } from './Charts/Stacked';
+export { Area } from './Charts/Area/Area';
+export { Bar } from './Charts/Bar/Bar';
+export { ColorMapping } from './Charts/ColorMapping/ColorMapping';
+export { Financial } from './Charts/Financial/Financial';
+export { Line } from './Charts/Line/Line';
+export { Pie } from './Charts/Pie/Pie';
+export { Pyramid } from './Charts/Pyramid/Pyramid';
+export { Stacked } from './Charts/Stacked/Stacked';
 
-export { ColorPicker } from './ColorPicker';
-export { Customers } from './Customers';
-export { Ecommerce } from './Ecommerce';
-export { Editor } from './Editor';
-export { Employees } from './Employees';
-export { Kanban } from './Kanban';
-export { Orders } from './Orders';
+export { ColorPicker } from './ColorPicker/ColorPicker';
+export { Customers } from './Customers/Customers';
+export { Ecommerce } from './Ecommerce/Ecommerce';
+export { Editor } from './Editor/Editor';
+export { Employees } from './Employees/Employees';
+export { Kanban } from './Kanban/Kanban';
+export { Orders } from './Orders/Orders';
