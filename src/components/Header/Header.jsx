dc3d66a446d4abc0b76e302412b650ca572f3054
torik135 <riqibrekele@gmail.com>
2022-09-09 17:28:34 +0700

    add: orders UI page & header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,11 @@
-const Header = () => {
-  return <div>Header</div>;
+const Header = (props) => {
+  const { category, title } = props;
+  return (
+    <div className='mb-10'>
+      <p className='text-gray-400'>{category}</p>
+      <p className='text-3xl font-extrabold tracking-light text-slate-900'>{title}</p>
+    </div>
+  );
 };
 
 export { Header };
--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,5 +1,49 @@
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from '@syncfusion/ej2-react-grids';
+
+import {
+  ordersData,
+  contextMenuItems,
+  ordersGrids,
+} from '../../data/dummyData';
+import { Header } from '../../components';
+
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div className='m-2 md:m-10 p-2 md:p-10 b-white rounded-3xl'>
+      <Header category='Page' title='orders' />
+      <GridComponent>
+        <ColumnsDirective id='gridcomp' dataSource={ordersData} allowPaging allowSorting>
+          {ordersGrids.map((item) => (
+            <ColumnDirective key={Math.random()} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Page,
+            Sort,
+            ContextMenu,
+            Filter,
+            ExcelExport,
+            PdfExport,
+            Edit,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
 };
 
 export { Orders };
