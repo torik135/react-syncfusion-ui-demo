9b4971c843d0c422fa91f05e54c9bf2c837418d2
torik135 <riqibrekele@gmail.com>
2022-09-09 18:17:27 +0700

    add: customer UI page

--- src/pages/Customers/Customers.jsx
+++ src/pages/Customers/Customers.jsx
@@ -1,5 +1,40 @@
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDIrective,
+  Page,
+  Selection,
+  Search,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+} from '@syncfusion/ej2-react-grids';
+
+import { customersData, customersGrid } from '../../data/dummyData';
+import { Header } from '../../components';
+
 const Customers = () => {
-  return <div>Customers</div>;
+  return (
+    <div className='m-2 md:m-10 p-2 md:p-10 b-white rounded-3xl'>
+      <Header category='Page' title='customer' />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        toolbar={['Search', 'Delete']}
+        editSettings={{allowEditing: true, allowDeleting: true}}
+        width='auto'
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item) => (
+            <ColumnDirective key={Math.random()} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar, Selection, Edit, Sort, Filter]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export { Customers };
