2114ab89d98f11748849ebf8341e48978d25bca2
torik135 <riqibrekele@gmail.com>
2022-09-09 20:01:00 +0700

    add: color picker UI page

--- src/pages/ColorPicker/ColorPicker.jsx
+++ src/pages/ColorPicker/ColorPicker.jsx
@@ -1,5 +1,45 @@
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
+
+import { Header } from '../../components';
+
+const changeColor = (args) => {
+  document.getElementById('preview').style.backgroundColor =
+    args.currentValue.hex;
+};
+
 const ColorPicker = () => {
-  return <div>ColorPicker</div>;
+  return (
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='App' title='Color Picker' />
+      <div className='text-center'>
+        <div id='preview' />
+        <div className='flex flex-wrap justify-center items-centergap-20'>
+          <div>
+            <p className='text-2xl font-semibold mt-2 mb-4'>Inline Pallete</p>
+            <ColorPickerComponent
+              id='inline-pallete'
+              mode='Pallete'
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+              change={changeColor}
+            />
+          </div>
+          <div>
+            <p className='text-2xl font-semibold mt-2 mb-4'>Inline Picker</p>
+            <ColorPickerComponent
+              id='inline-pallete'
+              mode='Picker'
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+              change={changeColor}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { ColorPicker };
