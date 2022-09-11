b3aa07426313e3ab8c33c32b01c5f0515fdf7493
torik135 <riqibrekele@gmail.com>
2022-09-11 19:46:07 +0700

    fix: typo and styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ const Header = (props) => {
   const { category, title } = props;
   return (
     <div className='mb-10'>
-      <p className='text-gray-400'>{category}</p>
+      <p className='text-gray-400 text-lg'>{category}</p>
       <p className='text-3xl font-extrabold tracking-light text-slate-900'>{title}</p>
     </div>
   );
