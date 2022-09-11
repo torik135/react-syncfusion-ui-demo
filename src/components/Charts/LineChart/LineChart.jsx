cc41a132a3dd1a186760a36d167db5247eacb610
torik135 <riqibrekele@gmail.com>
2022-09-11 20:41:33 +0700

    add: Line Chart UI page

--- src/components/Charts/LineChart/LineChart.jsx
+++ src/components/Charts/LineChart/LineChart.jsx
@@ -1,5 +1,42 @@
+import {
+  ChartComponent,
+  LineSeries,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  Tooltip,
+} from '@syncfusion/ej2-react-charts';
+
+import {
+  lineCustomSeries,
+  LinePrimaryXAxis,
+  LinePrimaryYAxix,
+} from '../../../data/dummyData';
+import { useStateContext } from '../../../context/GlobalProvider';
+
 const LineChart = () => {
-  return <div>LineChart</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <ChartComponent
+      id='line-chart'
+      height='420px'
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxix}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373e' : '#fff'}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item) => (
+          <SeriesDirective key={Math.random()} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export { LineChart };
--- src/pages/Charts/Line/Line.jsx
+++ src/pages/Charts/Line/Line.jsx
@@ -1,5 +1,17 @@
+import { Header, LineChart } from '../../../components';
+
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <div
+      className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'
+      style={{ color: currentMode === 'Dark' ? '#fff' : '#33373e' }}
+    >
+      <Header category='Chart' title='Line Chart' />
+      <div className='w-full'>
+        <LineChart />
+      </div>
+    </div>
+  );
 };
 
 export { Line };
