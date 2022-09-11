849181f2f62a9d806b96d7fe278e7cdc4ec652ee
torik135 <riqibrekele@gmail.com>
2022-09-11 20:50:40 +0700

    add: area chart UI page

--- src/pages/Charts/Area/Area.jsx
+++ src/pages/Charts/Area/Area.jsx
@@ -1,5 +1,48 @@
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  SplineAreaSeries,
+  Inject,
+  DateTime,
+  Legend,
+} from '@syncfusion/ej2-react-charts';
+
+import {
+  areaCustomSeries,
+  areaPrimaryXAxis,
+  areaPrimaryYAxix,
+} from '../../../data/dummyData';
+import { Header } from '../../../components';
+import { useStateContext } from '../../../context/GlobalProvider';
+
 const Area = () => {
-  return <div>Area</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <div
+      className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'
+      style={{ color: currentMode === 'Dark' ? '#fff' : '#33373e' }}
+    >
+      <Header category='Chart' title='Area Chart' />
+      <ChartComponent
+        id='area-chart'
+        height='420px'
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxix}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === 'Dark' ? '#33373e' : '#fff'}
+      >
+        <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item) => (
+            <SeriesDirective key={Math.random()} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export { Area };
