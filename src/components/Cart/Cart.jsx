f3d414272d41e5176d4af98f67d946d3ce931730
torik135 <riqibrekele@gmail.com>
2022-09-11 20:57:44 +0700

    add: cart navbar popup

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,81 @@
+import React from 'react';
+import { MdOutlineCancel as CancelIcon } from 'react-icons/md';
+import {
+  AiOutlinePlus as PlusIcon,
+  AiOutlineMinus as MinusIcon,
+} from 'react-icons/ai';
+
+import { useStateContext } from '../../contexts/GlobalProvider';
+import { cartData } from '../../data/dummyData';
+import { Button } from '../';
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const { currentColor } = useStateContext();
+
+  return (
+    <div className='bg-half-transparent w-full fixed nav-item top-0 right-0 '>
+      <div className='float-right h-screen  duration-1000 ease-in-out dark:text-gray-200 transition-all dark:bg-[#484B52] bg-white md:w-400 p-8'>
+        <div className='flex justify-between items-center'>
+          <p className='font-semibold text-lg'>Shopping Cart</p>
+          <Button
+            icon={<CartIcon />}
+            color='rgb(153, 171, 180)'
+            bgHoverColor='light-gray'
+            size='2xl'
+            borderRadius='50%'
+          />
+        </div>
+        {cartData?.map((item) => (
+          <div key={Math.random()}>
+            <div>
+              <div className='flex items-center   leading-8 gap-5 border-b-1 border-color dark:border-gray-600 p-4'>
+                <img className='rounded-lg h-80 w-24' src={item.image} alt='' />
+                <div>
+                  <p className='font-semibold '>{item.name}</p>
+                  <p className='text-gray-600 dark:text-gray-400 text-sm font-semibold'>
+                    {item.category}
+                  </p>
+                  <div className='flex gap-4 mt-2 items-center'>
+                    <p className='font-semibold text-lg'>{item.price}</p>
+                    <div className='flex items-center border-1 border-r-0 border-color rounded'>
+                      <p className='p-2 border-r-1 dark:border-gray-600 border-color text-red-600 '>
+                        <MinusIcon />
+                      </p>
+                      <p className='p-2 border-r-1 border-color dark:border-gray-600 text-green-600'>
+                        0
+                      </p>
+                      <p className='p-2 border-r-1 border-color dark:border-gray-600 text-green-600'>
+                        <PlusIcon />
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+        <div className='mt-3 mb-3'>
+          <div className='flex justify-between items-center'>
+            <p className='text-gray-500 dark:text-gray-200'>Sub Total</p>
+            <p className='font-semibold'>$890</p>
+          </div>
+          <div className='flex justify-between items-center mt-3'>
+            <p className='text-gray-500 dark:text-gray-200'>Total</p>
+            <p className='font-semibold'>$890</p>
+          </div>
+        </div>
+        <div className='mt-5'>
+          <Button
+            color='white'
+            bgColor={currentColor}
+            text='Place Order'
+            borderRadius='10px'
+            width='full'
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { Cart };
